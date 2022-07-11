9bc8b8ab24128a11b38bd1afb678c37198926d4b
chcc <601553044@qq.com>
2022-07-11 18:31:10 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,132 @@
-from .request import login, followMember
+from request import login, followMember, saleMember, submit
+import datetime
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
-def login():
-    username = "lxy15020712608"
-    password = "910216"
-    cookie = login(username, password)
-    list = followMember(cookie=cookie, page=1)
+
+class Stream(QObject):
+    """Redirects console output to text widget."""
+    newText = pyqtSignal(str)
+
+    def write(self, text):
+        self.newText.emit(str(text))
+        QApplication.processEvents()
+
+
+class MainUi(QWidget):
+    def __init__(self):
+        super(MainUi, self).__init__()
+        # 注掉这句就可以打印到控制台，方便调试
+        # sys.stdout = Stream(newText=self.onUpdateText)
+        self.initGUI()
+
+    def initGUI(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setWindowTitle("三体云动会员跟进辅助程序")
+        self.setFixedSize(600, 800)
+
+        # 外层容器
+        container = QVBoxLayout()
+
+        # 表单容器
+        formLayout = QFormLayout()
+
+        rightLayout = QStackedLayout()
+        self.process = QTextEdit(self, readOnly=True)
+        self.process.ensureCursorVisible()
+        self.process.setLineWrapColumnOrWidth(500)
+        self.process.setLineWrapMode(QTextEdit.FixedPixelWidth)
+        rightLayout.addWidget(self.process)
+
+        # 账号
+        self.usernameInput = QLineEdit()
+        self.usernameInput.setPlaceholderText("请输入用户名")
+
+        # 密码输入框
+        self.paswwordInput = QLineEdit()
+        self.paswwordInput.setPlaceholderText("请输入密码")
+
+        # 页码
+        self.start = QLineEdit()
+        self.end = QLineEdit()
+
+        # 内容输入框
+        self.content = QLineEdit()
+        self.content.setPlaceholderText("请输入跟进内容")
+        self.content.setFixedSize(500, 100)
+
+        formLayout.addRow("账号:", self.usernameInput)
+        formLayout.addRow("密码", self.paswwordInput)
+        formLayout.addRow("开始", self.start)
+        formLayout.addRow("结束", self.end)
+        formLayout.addRow("内容", self.content)
+
+
+        btn = QPushButton("开始")
+        btn.setFixedSize(100, 50)
+        btn.clicked.connect(self.OnBtnClicked)
+
+        widgetRight = QWidget()
+        widgetRight.setLayout(rightLayout)
+        widgetRight.setStyleSheet("background-color:grey;")
+
+        widgetLeft = QWidget()
+        widgetLeft.setLayout(formLayout)
+
+        container.addWidget(widgetLeft, alignment=Qt.AlignLeft)
+        container.addWidget(widgetRight)
+        container.addWidget(btn, alignment=Qt.AlignRight)
+
+        self.setLayout(container)
+
+    def onUpdateText(self, text):
+        """Write console output to text widget."""
+        cursor = self.process.textCursor()
+        cursor.movePosition(QTextCursor.End)
+        cursor.insertText(text)
+        self.process.setTextCursor(cursor)
+        self.process.ensureCursorVisible()
+
+    def closeEvent(self, event):
+        """Shuts down application on close."""
+        # Return stdout to defaults.
+        sys.stdout = sys.__stdout__
+        super().closeEvent(event)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def OnBtnClicked(self):
+        content = self.content.text()
+        username = self.usernameInput.text()
+        password = self.paswwordInput.text()
+        end = int(self.end.text())
+        start = int(self.start.text())
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        cookie = login(username, password)
+        list = followMember(cookie, start)
+        staff_id = None
+        while len(list) > 0 and start < end:
+            for memberId in list:
+                if staff_id is None:
+                    staff_id = saleMember(cookie, memberId)
+                submit(cookie, staff_id, time, content, memberId)
+            print("当前页码 %d" % start)
+            start += 1
+            list = followMember(cookie, start)
+
 
 if __name__ == '__main__':
-    login()
+    app = QApplication(sys.argv)
+    ui = MainUi()
+    ui.show()
+    ui.usernameInput.setText("lxy15020712608")
+    ui.paswwordInput.setText("910216")
+    ui.start.setText("1")
+    ui.end.setText("9999")
+    sys.exit(app.exec_())
--- request.py
+++ request.py
@@ -3,27 +3,84 @@ import urllib
 from bs4 import BeautifulSoup
 import json
 
-COOKIE_NAME = "wsToken"
-HEAD = {"Content-Type": "application/json", "Connection": "keep-alive"}
+COOKIE_NAME = "ran_sass_gym"
+HEAD = {"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Connection": "keep-alive",
+        "Host": "www.styd.cn",
+        "Origin": "https://www.styd.cn",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
+        }
 
-def login(username,password):
+
+# 登录系统
+def login(username, password):
     url = "https://www.styd.cn/web/user/do_login"
     data = {
         "login_name": username,
         "login_pwd": password
     }
-    res = requests.post(url, json=data)
-    cookie = res.cookies[COOKIE_NAME]
+    res = requests.post(url, data=data, headers=HEAD)
+    print("登录接口返回状态{%s}" % json.loads(res.content))
+    cookie = res.cookies
     return cookie
 
+
+# 获取会员列表
 def followMember(cookie, page=1):
-    url = "https://www.styd.cn/club/member/sale_follow_member?mix_cond=&p=" + page
+    url = "https://www.styd.cn/club/member/sale_follow_member?mix_cond=&p=" + str(page)
     result = requests.get(
         url,
-        cookies=cookie)
-    soup = BeautifulSoup(result.content, 'lxml')
-    print("获取返回数据 %s" % (soup.get_text()))
-    memberIdList = soup.find_all(name="member_id")
-    print("获取返回数据 %s" % (str(memberIdList)))
-    return memberIdList
+        cookies=cookie,
+        headers=HEAD)
+    print("获取会员列表接口返回状态{%s}" % result)
+    soup = BeautifulSoup(result.content, 'html.parser')
+    soup = soup.find_all(name="input", attrs={"name": "member_id"})
+    member_id_list = []
+    for s in soup:
+        value = s.get_attribute_list('value')
+        member_id_list.append(int(value.pop()))
+    print("获取返回数据 %s" % member_id_list)
+    return member_id_list
 
+
+# 获取员工id
+def saleMember(cookie, memberId):
+    url = "https://www.styd.cn/club/member/follow?type=sale_member&member_id=" + str(memberId)
+    data = {
+        "type": "sale_member",
+        "member_id": memberId
+    }
+    result = requests.get(
+        url,
+        data = data,
+        cookies=cookie,
+        headers=HEAD)
+    print("获取会员信息接口返回状态{%s}" % result)
+    soup = BeautifulSoup(json.loads(result.content).get('data'), 'html.parser')
+    userIdHtml = soup.find_all(name="input", attrs={"name": "current_user_id"})
+    for s in userIdHtml:
+        value = s.get_attribute_list('value')
+        return str(value.pop())
+
+
+# 提交跟进信息
+def submit(cookie, staff_id, time, content, memberId, concat_type=1, contact_status=1, contact_result=1):
+    url = "https://www.styd.cn/club/member/follow"
+    data = {
+        "shop_id": 178296502,
+        "member_id": memberId,
+        "type": "sale_member",
+        "staff_id": staff_id,  # 员工id
+        "contact_type": concat_type,  # 服务方式 1电话跟进 2短信跟进 3见面跟进 4email跟进
+        "contact_status": contact_status,  # 通讯状态 1接通 0无人接听 2电话忙 3空号 4关机 5挂断 6停机
+        "contact_result": contact_result,  # 通讯结果 1预约成功
+        "fail_reason": 0,
+        "sales_id": staff_id,  # 员工id
+        "time": time,
+        "class_id": "请选择",
+        "content": content,
+        "next_contact": ""
+    }
+    print("提交数据 %s" % data)
+    res = requests.post(url, cookies=cookie, data=data, headers=HEAD)
+    print("提交会员跟进信息返回{%s}" % json.loads(res.content))
